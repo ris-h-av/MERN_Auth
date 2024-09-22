266a47d7dd3fcd8804d0a175dca8e46c39a6552d
ris-h-av <risabk444@gmail.com>
2024-09-22 12:49:36 +0530

    added pre-production changes

--- api/index.js
+++ api/index.js
@@ -4,6 +4,7 @@ import dotenv from 'dotenv';
 import userRoutes from './routes/user.routes.js';
 import authRoutes from './routes/auth.route.js';
 import cookieParser from 'cookie-parser';
+import path from 'path';
 dotenv.config();
 
 mongoose.connect(process.env.MONGO).then(()=>{
@@ -12,7 +13,15 @@ mongoose.connect(process.env.MONGO).then(()=>{
     console.log(err);
 });
 
+const __dirname = path.resolve(); // this will find the dynamic directory name in any place or any server
+
 const app = express();
+
+app.use(express.static(path.join(__dirname,'/client/dist')));
+
+app.get('*', (req,res)=>{
+    res.sendFile(path.join(__dirname, 'client','dist','index.html'));
+});
 
 app.use(express.json());
 
